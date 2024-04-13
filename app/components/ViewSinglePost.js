191d5073decadd9dc3a5f645b64d23a8ea1ee93f
joeyparagas <joey.paragas@gmail.com>
2024-04-13 15:43:21 -0700

    Output db post data to page, update single post page

--- app/components/ViewSinglePost.js
+++ app/components/ViewSinglePost.js
@@ -1,11 +1,49 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Page from "./Page";
+import Axios from "axios";
+import { useParams, Link } from "react-router-dom";
 
 function ViewSinglePost() {
+    // Get id number from URL
+    const { id } = useParams();
+    // State for initial loading of post
+    const [isLoading, setIsLoading] = useState(true);
+    // Updates the post on the page
+    const [post, setPost] = useState(true);
+
+    // Pull post from db server via Axios
+    useEffect(() => {
+        async function fetchPost() {
+            try {
+                const response = await Axios.get(`/post/${id}`);
+                setPost(response.data);
+                setIsLoading(false); // set setIsLoading to false skip loading... screen
+            } catch (e) {
+                console.log("There was a problem fetching posts!");
+            }
+        }
+        fetchPost();
+    }, []);
+
+    // Initial loading of page
+    if (isLoading) {
+        return (
+            <Page title="...">
+                <div>Loading...</div>
+            </Page>
+        );
+    }
+
+    // Get and format post dates
+    const date = new Date(post.createdDate);
+    const dateFormatted = `${
+        date.getMonth() + 1
+    }/${date.getDate()}/${date.getFullYear()}`;
+
     return (
-        <Page title="Fake Hardcoded Title">
+        <Page title={post.title}>
             <div className="d-flex justify-content-between">
-                <h2>Example Post Title</h2>
+                <h2>{post.title}</h2>
                 <span className="pt-2">
                     <a href="#" className="text-primary mr-2" title="Edit">
                         <i className="fas fa-edit"></i>
@@ -20,33 +58,17 @@ function ViewSinglePost() {
             </div>
 
             <p className="text-muted small mb-4">
-                <a href="#">
-                    <img
-                        className="avatar-tiny"
-                        src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"
-                    />
-                </a>
-                Posted by <a href="#">brad</a> on 2/10/2020
+                <Link to={`/profile/${post.author.username}`}>
+                    <img className="avatar-tiny" src={post.author.avatar} />
+                </Link>
+                Posted by{" "}
+                <Link to={`/profile/${post.author.username}`}>
+                    {post.author.username}
+                </Link>{" "}
+                on {dateFormatted}
             </p>
 
-            <div className="body-content">
-                <p>
-                    Lorem ipsum dolor sit <strong>example</strong> post
-                    adipisicing elit. Iure ea at esse, tempore qui possimus
-                    soluta impedit natus voluptate, sapiente saepe modi est
-                    pariatur. Aut voluptatibus aspernatur fugiat asperiores at.
-                </p>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Beatae quod asperiores corrupti omnis qui, placeat neque
-                    modi, dignissimos, ab exercitationem eligendi culpa
-                    explicabo nulla tempora rem? Lorem ipsum dolor sit amet
-                    consectetur adipisicing elit. Iure ea at esse, tempore qui
-                    possimus soluta impedit natus voluptate, sapiente saepe modi
-                    est pariatur. Aut voluptatibus aspernatur fugiat asperiores
-                    at.
-                </p>
-            </div>
+            <div className="body-content">{post.body}</div>
         </Page>
     );
 }
